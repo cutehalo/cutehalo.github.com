e0c7d28928a073d909de570d32e219e71412788d
cutehalo <cutehalo@gmail.com>
2013-09-21 13:44:42 +0800

    add lab category

---
+++ lab/lab.js
@@ -0,0 +1,22 @@
+var arr = [];
+$("div.playlist").each(function(){
+	var text = $(this).next().next().next().next().next().text();
+	var reg = /song_records = (.*);/g
+	var t = text.match(reg);
+	var b =t[0].replace("song_records = ","");
+	var b = strToJson(b);
+	for (var i = 0; i < b.length; i++) {
+		arr.push(b[i].rawUrl);
+	};
+})
+
+function strToJson(str){ 
+	var json = (new Function("return " + str))(); 
+	return json; 
+};
+$("tr[class^=tr]").each(function(i){
+	if (arr.length==$("tr[class^=tr]").length) {
+		var html = '<a target="_blank" href="'+arr[i]+'">右键保存</a>';
+		$(this).find("td.title").append(html);
+	};
+})
