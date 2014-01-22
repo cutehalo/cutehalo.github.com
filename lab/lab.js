72ef3cd62c5902e4a9e704edf41a90ab91791a35
cutehalo <cutehalo@gmail.com>
2014-01-22 16:48:08 +0800

    update douban mp3 downloader

--- lab/lab.js
+++ lab/lab.js
@@ -1,26 +1,15 @@
-var arr = [];
 if (location.href.indexOf("site.douban.com")>-1) {
-	$("div.playlist").each(function(){
-		var text = $(this).next().next().next().next().next().text();
-		var reg = /song_records = (.*);/g
-		var t = text.match(reg);
-		var b =t[0].replace("song_records = ","");
-		var b = strToJson(b);
-		for (var i = 0; i < b.length; i++) {
-			arr.push(b[i].rawUrl);
+	$("div[id^=playlist]").each(function(){
+	var o = $(this);
+	var id = o.attr("id").split("-")[1];
+	var songArr = PlaylistWidget.findOrCreate(id).songs;
+	o.find("tr[class^=tr]").each(function(i){
+		if (songArr.length==o.find("tr[class^=tr]").length) {
+			var html = '<a href="'+songArr[i].rawUrl+'">Save</a>';
+			$(this).find("td.title").prepend(html);
 		};
 	})
-
-	function strToJson(str){ 
-		var json = (new Function("return " + str))(); 
-		return json; 
-	};
-	$("tr[class^=tr]").each(function(i){
-		if (arr.length==$("tr[class^=tr]").length) {
-			var html = '<a href="'+arr[i]+'">Save</a>';
-			$(this).find("td.title").append(html);
-		};
-	})
+})
 }
 else{
 	alert("You should click this bookmark when you are browsing a site of Douban.");
