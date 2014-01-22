552893d66df4dc46e9a283816d74b2b8ac85ec43
cutehalo <cutehalo@gmail.com>
2014-01-22 16:54:41 +0800

    update douban mp3 downloader

--- lab/lab.js
+++ lab/lab.js
@@ -1,15 +1,15 @@
 if (location.href.indexOf("site.douban.com")>-1) {
 	$("div[id^=playlist]").each(function(){
-	var o = $(this);
-	var id = o.attr("id").split("-")[1];
-	var songArr = PlaylistWidget.findOrCreate(id).songs;
-	o.find("tr[class^=tr]").each(function(i){
-		if (songArr.length==o.find("tr[class^=tr]").length) {
-			var html = '<a href="'+songArr[i].rawUrl+'">Save</a>';
-			$(this).find("td.title").prepend(html);
-		};
+		var o = $(this);
+		var id = o.attr("id").split("-")[1];
+		var songArr = PlaylistWidget.findOrCreate(id).songs;
+		o.find("tr[class^=tr]").each(function(i){
+			if (songArr.length==o.find("tr[class^=tr]").length) {
+				var html = '<a href="'+songArr[i].rawUrl+'">Save</a>';
+				$(this).find("td.title").prepend(html);
+			};
+		})
 	})
-})
 }
 else{
 	alert("You should click this bookmark when you are browsing a site of Douban.");
